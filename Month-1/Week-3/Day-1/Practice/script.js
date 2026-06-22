bec1b6b7345f038df889d2a2e3ac719fbeb8021c
Developer-soumya <soumyamaharana89@gmail.com>
2026-06-22 13:09:26 +0530

    js start

---
+++ Month-1/Week-3/Day-1/Practice/script.js
@@ -0,0 +1,57 @@
+console.log("hello World");
+
+a=30;
+console.log(a);
+
+A=40;
+console.log(A);
+
+st1="subhankar";
+console.log(st1);
+
+_=80;
+console.log(_)
+
+$=70;
+console.log($)
+
+
+full_name="hii";
+console.log(full_name);
+
+// var let const
+// scope
+// global
+// function
+// block
+
+// declare at global level
+var a=40;
+
+console.log(a)
+
+// declare at block level
+{
+    var b=50;
+    console.log("block level access",b);
+}
+
+console.log("outside the block",b);
+
+// declare at function level
+function getV(){
+    var d=55;
+    console.log(d)
+}
+
+// console.log(d)
+
+getV()
+
+
+
+
+
+
+
+
