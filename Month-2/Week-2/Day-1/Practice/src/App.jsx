ae7435649070b854708826267cbfebbd01aac9cc
Developer-soumya <soumyamaharana89@gmail.com>
2026-07-06 13:08:49 +0530

    react setup done

---
+++ Month-2/Week-2/Day-1/Practice/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <div>
+      App
+      <br />
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
