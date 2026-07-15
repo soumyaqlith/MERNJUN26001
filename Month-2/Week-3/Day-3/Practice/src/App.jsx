73eed46312bc69dd40b30debc5a9b3660e335d36
Developer-soumya <soumyamaharana89@gmail.com>
2026-07-15 14:08:02 +0530

    react project neltify deploy

---
+++ Month-2/Week-3/Day-3/Practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>This is my first react deployment</div>
+      <div>This is my second react deployment</div>
+    </>
+  );
+}
+
+export default App;
